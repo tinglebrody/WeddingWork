5e2ab60b2cf81b0db318ca7847f1f75a674f5ae5
tinglebrody <tinglebrody@gmail.com>
2023-10-03 15:20:17 -0400

    starting vendors

---
+++ Vendors.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.imageio.ImageIO;
+import java.io.*;
+import java.awt.image.BufferedImage;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Vendors implements ActionListener{
+    JPanel inputPanel;
+    JLabel nameLabel, jobLabel, phoneLabel, emailLabel;
+    JTextField nameInput, jobInput, phoneInput, emailInput;
+    JButton addButton;
+    ArrayList<JLabel> vendorList;
+    GridBagConstraints constraints;
+
+    public void actionPerformed(ActionEvent event){}
+}
+
