eb4e873439df8a9f9a69f1f7afb15c2a62085cee
tinglebrody <tinglebrody@gmail.com>
2023-10-03 11:15:25 -0400

    home looks better

--- Home.java
+++ Home.java
@@ -4,33 +4,22 @@ import java.awt.event.*;
 import javax.imageio.ImageIO;
 import java.io.*;
 import java.awt.image.BufferedImage;
-public class Home implements ActionListener{
+public class Home extends Page implements ActionListener{
 
-JPanel panel;
-JLabel homeLabel, welcomeLabel, photo;
-JButton button;
+JLabel welcomeLabel;
 
     public Home() throws IOException{
-
-        welcomeLabel = new JLabel("Welcome to WeddingWork!");
-        homeLabel = new JLabel("Home");
-        button = new JButton("Click to accomplish (almost) nothing");
-        Color buttonColor = new Color(255,255,245);
-        button.setBackground(buttonColor);
         panel = new JPanel();
-        panel.setBackground(buttonColor);
+        constraints = new GridBagConstraints();
+        implementPanel(panel);
 
-        button.addActionListener(this);
-
-        panel.setBorder(BorderFactory.createEmptyBorder(600,600,500,500));
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-        panel.add(homeLabel);
-        panel.add(welcomeLabel);
-        panel.add(button);
-        panel.setVisible(false);
+        welcomeLabel = new JLabel("Welcome " + super.username + "!");
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        panel.add(welcomeLabel, constraints);
     }
 
     public void actionPerformed(ActionEvent e){
-        homeLabel.setText("Home Page");
+        
     }
 }
