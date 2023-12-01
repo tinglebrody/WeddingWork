3dd8990705ad5ac8a067b25c0e9d80e330fa1cae
tinglebrody <tinglebrody@gmail.com>
2023-11-30 23:42:29 -0500

    Added description to home

--- Home.java
+++ Home.java
@@ -6,7 +6,7 @@ import java.io.*;
 import java.awt.image.BufferedImage;
 public class Home extends Page{
 
-private JLabel welcomeLabel, usernameLabel;
+private JLabel welcomeLabel, usernameLabel, descriptionLabel;
 
     public Home() throws IOException{
         panel = new JPanel();
@@ -17,11 +17,16 @@ private JLabel welcomeLabel, usernameLabel;
         welcomeLabel.setFont(superFont);
         usernameLabel = new JLabel("Welcome " + super.username + "!");
         usernameLabel.setFont(bigFont);
+        descriptionLabel = new JLabel("Use Navigate to Explore Pages!");
+        descriptionLabel.setFont(smallerFont);
         constraints.gridx = 0;
         constraints.gridy = 0;
         panel.add(welcomeLabel, constraints);
         constraints.gridx = 0;
         constraints.gridy = 1;
         panel.add(usernameLabel, constraints);
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        panel.add(descriptionLabel, constraints);
     }
 }
